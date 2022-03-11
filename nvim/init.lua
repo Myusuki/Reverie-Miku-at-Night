e3ce0f82cade267b55cbeaa8cd2e941e8e7fdc1d
Myusuki <myusuki_86666@tutanota.com>
2022-03-11 14:23:35 -0500

    commiting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,88 +1,23 @@
-vim.o.clipboard = 'unnamedplus'
-vim.wo.number = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.laststatus = 2
-vim.o.showmode = false
-vim.wo.wrap = true
-vim.wo.linebreak = true
-vim.wo.list = false
-vim.o.showtabline = 2
+vim.o.clipboard = 'unnamedplus' -- clipboard
+vim.wo.number = true -- number lines on the side
+vim.o.splitright = true -- split windows to the right 
+vim.wo.wrap = true -- softwrap 
+vim.o.showtabline = 2 -- shows the tabline
 vim.bo.syntax = 'ON'
 vim.o.hlsearch = true
-vim.o.completeopt = "menu,menuone,noselect"
+-- tab length
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.expandtab = true
-vim.bo.autoindent = true
 
 local map = vim.api.nvim_set_keymap
--- leader key 
+-- leader key
 vim.g.mapleader = '`'
 
 -- leader hotkey functions
-options = {noremap = true}
+local options = {noremap = true}
 map('n', '<leader>so', ':so $MYVIMRC<cr>', options)
-map('n', '<leader>ps', ':PlugInstall<cr>', options)
-map('n', '<leader>pc', ':PlugClean<cr>', options)
-map('n', '<leader>li', ':LspInstallInfo<cr>', options)
+-- map('n', '<leader>ls', ':LspStart<cr>', options)
+-- map('n', '<leader>li', ':LspInstallInfo<cr>', options)
+-- map('n', '<leader>z', ':TZAtaraxis<CR>', options)
 
-local Plug = vim.fn['plug#']
-vim.call('plug#begin', '~/.vim/plugged')
--- cosmetics
-Plug 'dylanaraps/wal.vim'
-Plug 'ap/vim-css-color'
-Plug 'lukas-reineke/indent-blankline.nvim'
--- tools
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'junegunn/goyo.vim'
-Plug 'dense-analysis/ale'
--- autocompletion
-Plug 'neovim/nvim-lspconfig'
-Plug 'williamboman/nvim-lsp-installer'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'SirVer/ultisnips'
-Plug 'quangnguyen30192/cmp-nvim-ultisnips'
-Plug 'honza/vim-snippets'
-Plug 'Raimondi/delimitMate'
-vim.call('plug#end')
-
-require('nvim-cmp')
-require('lsp')
-require('indentline')
-require('ale')
-
--- lualine
-require('lualine').setup {
-	options = {
-		icons_enabled = true,
-		theme = 'Miku',
-		component_separators = '|',
-		section_separators = { left = '', right = ''},
-		disabled_filetypes = {},
-		always_divide_middle = true,
-		},
-	sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_c = {'filename'},
-		lualine_x = {'progress'},
-		lualine_y = {'encoding', 'fileformat', 'filetype'},
-		lualine_z = {'location'},
-		},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
-		lualine_y = {},
-		lualine_z = {},
-		},
-	tabline = { lualine_a = {'tabs', 'filename'}, },
-	extensions = {},
-}
